477354d8d77a644eb30bbf85e23286932971165c
maneesha herath <72514560+Maneeshaprabha@users.noreply.github.com>
2025-02-12 20:05:37 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -8,7 +8,7 @@ document.getElementById("contact-form").addEventListener("submit", async functio
     };
 
     try {
-        const response = await fetch("https://maneeshaprabha.github.io/playmadefontend/send-email", {
+        const response = await fetch("http:localhost:3000/send-email", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
