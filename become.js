2a55c9b392da67938402249d1c3241330493695d
maneesha herath <72514560+Maneeshaprabha@users.noreply.github.com>
2025-02-12 20:06:07 +0530

    Update become.js

--- become.js
+++ become.js
@@ -23,7 +23,7 @@ document.querySelector("#partner-form").addEventListener("submit", async (event)
     console.log("Form data to send:", formData);
 
     try {
-        const response = await fetch("https://github.com/uxboxartz/playmade.git/become-partner", {
+        const response = await fetch("http://localhost:3000/become-partner", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
